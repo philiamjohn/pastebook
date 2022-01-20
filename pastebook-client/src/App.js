557586024ec8de975f3b8f5c481ffc4131c8ec1f
philiamjohn <philiamjohnimpas@gmail.com>
2022-01-20 11:28:22 +0800

    ADDED: initial push

---
+++ pastebook-client/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+    </Routes>
+  );
+};
+
+
+export default App;
---
+++ pastebook-client/src/pages/home/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = () => {
+  return <div>Pastebook Home</div>;
+};
+
+export default Home;
