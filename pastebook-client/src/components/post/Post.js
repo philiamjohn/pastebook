4b8a0f3cdf6d4bae438ec561bf322d2dff7a57da
gavitons <gavintarega@gmail.com>
2022-01-21 22:18:30 +0800

    Updated Post Component, Added some icons
    1.Updated Post Component
    -created a like-unlike toggle functionality for like button (complete)
    -created a show/hide toggle functionality for comments (partial)
    -added the post-comments portion(partial)
    -modified the post-content image so that a gray stock photo temporarily serves as a placeholder while waiting for data
    2.Added some icons and images
    -like and liked icons
    -comment icon
    -gray stock photo
    
    Additional description for the previous commit:
    1. Added Post component : (Components/Post.js and Components/Post.css)
    -Post Author, contents, and interactions portions(complete)
    2. Added Post page: (Pages/Post.js and Pages/Post.css)
    3. Modified App.js : added /post route
    4. Added some post stock images for visualization

--- pastebook-client/src/components/post/Post.js
+++ pastebook-client/src/components/post/Post.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Post.css';
+import LikeIcon from '../../images/like.png';
+import LikedIcon from '../../images/liked.png';
+import CommentIcon from '../../images/comment.png';
+import GrayStock from '../../images/gray.jpg';
 
 const Post = (props) => {
     
-    const {authorImg,authorName,postTimeStamp,postContentP,postContentImg,likeCount,commentCount} = props
+    const {authorImg,
+           authorName,
+           postTimeStamp,
+           postContentP,
+           postContentImg,
+           likeCount,
+           commentCount,
+           likeStatus} = props
+    
+    const [like, setLike] = useState(likeStatus);
+    const [comment, setComment] = useState(commentCount); 
 
+    // like/unlike  toggle
+    const toggleLike = () => {
+        if(like){
+            window.alert("Like has been undone!");
+        }
+        else{
+            window.alert("Liked!");
+        }
+        setLike(v => !v);
+    };
+
+    // show/hide comment toggle
+    const toggleComment = () => {
+        setComment(v => !v);
+    }
+    
     return (
         <div className='post'>
             <div className='post-author'>
                 <div className='post-author-img'>
-                    <a href="#"><img src={authorImg} alt="author-img"/></a>
+                    <a href="#"><img src={authorImg ? authorImg  : GrayStock } alt="author-img"/></a>
                 </div>
                 <div className='post-author-details'>
                     <div className='post-author-details-name'><a href="#"><h4>{authorName}</h4></a></div>
@@ -18,18 +48,34 @@ const Post = (props) => {
             </div>
             <div className='post-content'>
                 <div className='post-content-p'>{postContentP}</div>
-                <div className='post-content-img'><img src={postContentImg} alt="content-img"/></div>         
+                <div className='post-content-img'><img src={postContentImg? postContentImg : GrayStock } alt="content-img"/></div>         
             </div>
             <div className='post-interactions'>
                 <div className='post-interactions-counts'>
                     <div className='post-interactions-counts-like'><a href='#'>{likeCount} Likes</a></div>
-                    <div className='post-interactions-counts-like'></div><a href='#'>{commentCount} Comments</a>
+                    <div className='post-interactions-counts-comment'></div><a href="#" onClick={toggleComment}>{commentCount} Comments</a>
                 </div>
                 <div className='post-interactions-btns'>
-                    <button>Like</button>
-                    <button>Comment</button>
+                    <div className='post-interactions-btns-like' onClick={toggleLike}>
+                        <img src={like? LikedIcon : LikeIcon} alt='like-icon'/>
+                        Like
+                    </div>
+                    <div className='post-interactions-btns-comment' onClick={comment}>
+                        <img src={CommentIcon} alt='comment-icon'/>
+                        Comment
+                    </div>
                 </div>
                 <div className='post-interactions-comment'></div>
+            </div>
+            <div className='post-comments'>
+                <div className='post-comments-expand'><a href='#'>View more comment</a></div>
+                <div className='post-comments-list'>
+                    
+                </div>
+                <div className='post-comments-create'>
+                    <img src={authorImg ? authorImg  : GrayStock } alt="author-img"/>
+                    <input type='text' placeholder="Write a comment"/>
+                </div>
             </div>
         </div>
     );
