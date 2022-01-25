1e983d1fa222247d1069ecc602b848d7d8f8bac5
philiamjohn <philiamjohnimpas@gmail.com>
2022-01-25 14:30:45 +0800

    ADDED: Backend For Login

---
+++ PastebookServer/Models/UserCredentialsModel.cs
@@ -0,0 +1,6 @@
+public class UserCredentialsModel
+{
+    public string? Email { get; set; }
+    public string? Phone { get; set; }
+    public string? Password { get; set; }
+}
--- PastebookServer/PastebookController.cs
+++ PastebookServer/PastebookController.cs
@@ -2,13 +2,30 @@ using Microsoft.AspNetCore.Mvc;
 
 public class PastebookController : Controller
 {
-    // [HttpGet]
-    // [Route("/home")]
-    // public IActionResult getHomeData(
-    //     [FromHeader(Name = "X-SessionID")] string pastebookSessionId
-    // )
-    // {
-    //     string currentUser = Database.GetSessionById(pastebookSessionId);
-    //     return Ok(Json(homeData));
-    // }
+    [HttpGet]
+    [Route("/home")]
+    public IActionResult getHomeData(
+        [FromHeader(Name = "X-SessionID")] string pastebookSessionId
+    )
+    {
+        SessionModel session = Database.GetSessionById(pastebookSessionId)!;
+        if (session != null)
+        {
+            return Ok(Json(session));
+        }
+        return Unauthorized();
+    }
+    [HttpPost]
+    [Route("/login")]
+    public IActionResult loginAddSessionForUser(
+        [FromBody] UserCredentialsModel userCredentials
+    )
+    {
+        SessionModel session = Database.AddSessionWithCredentials(userCredentials)!;
+        if (session != null)
+        {
+            return Ok(Json(session));
+        }
+        return Unauthorized();
+    }
 }
