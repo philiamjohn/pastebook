5cbd91beeffcb57dc719214a7ccbe377905dc2ca
kelyn-it <jacqueline.b.bernal@gmail.com>
2022-01-25 12:43:26 +0800

    ADD: Album Page and CSS
    - basic grid of albums that automatically positions when a new album is created
    - no working functions yet

--- pastebook-client/src/App.js
+++ pastebook-client/src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
+import Album from './pages/album/Album';
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/login' element={<Login />} />
+      <Route path='/username/photos' element={<Album />} />
     </Routes>
   );
 };
