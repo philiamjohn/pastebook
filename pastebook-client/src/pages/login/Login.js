e57d5755661263e188eb224f00e58473e00741bd
philiamjohn <philiamjohnimpas@gmail.com>
2022-01-27 16:30:29 +0800

    UPDATE: ClearInterval
    UPDATE: ClearInterval

--- pastebook-client/src/pages/login/Login.js
+++ pastebook-client/src/pages/login/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../login/Login.css';
 import logo from '../../images/pb.png'
 import { useNavigate } from 'react-router-dom';
