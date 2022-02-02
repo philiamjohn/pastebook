2f8c1c6b381b1e5683b7360957366edfbf354828
philiamjohn <philiamjohnimpas@gmail.com>
2022-02-02 15:36:54 +0800

    UPDATE: Like and Comment Notification; Search Profile Picture

--- pastebook-client/src/components/notifications/comment-notification/CommentNotification.js
+++ pastebook-client/src/components/notifications/comment-notification/CommentNotification.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { BiCommentDetail } from 'react-icons/bi';
+import { BsCheckLg } from 'react-icons/bs';
 
 const CommentNotification = (props) => {
-    const { commenter } = props;
+    const { notification, makeNotificationRead } = props;
     return (
-        <p>
+        <p className={notification.ReadStatus === "unread" ? 'notification-unread' : ''}>
             <BiCommentDetail
                 id="comment-icon"
                 size={15}
                 color='black' />
-            {commenter} commented on your post.
+            <Link to={`/posts/${notification.Content}`} className='notification-link'>{notification.Name} commented your post.</Link>
+            {
+                notification.ReadStatus === "unread"
+                    ?
+                    <button className="notification-clear-button" onClick={() => makeNotificationRead(notification.Notification_ID)}>
+                        <BsCheckLg
+                            className="notification-clear-icon"
+                            size={15}
+                            color='rgb(0, 165, 22)' />
+                    </button>
+                    : <div></div>
+            }
+            <div className="notification-date-triggered">{notification.DateTriggered}</div>
         </p>);
 };
 
