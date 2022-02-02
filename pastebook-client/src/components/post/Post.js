344910bbe473c62f34957dd6266f967cd27f9e0a
gavitons <gavintarega@gmail.com>
2022-02-02 13:19:32 +0800

    Update: Database primary id name change adjustment

--- pastebook-client/src/components/post/Post.js
+++ pastebook-client/src/components/post/Post.js
@@ -112,7 +112,8 @@ const Post = (props) => {
                 }
             });
             if (response.status === 200) {
-                setLikeStatus(v => !v);  
+                setLikeStatus(v => !v);
+                fetchLikes();
             }
             else {
               alert("Failed to like post "+postID);
@@ -131,15 +132,32 @@ const Post = (props) => {
             });
             if (response.status === 200) {
                 setLikeStatus(v => !v);  
+                fetchLikes();
             }
             else {
               alert("Failed unlike post "+postID);
             }
           }
     }
+
+    const fetchLikes = () => {
+        //fetch likes info
+        if(postID!=null){
+            fetch(`${baseUrl}/postLikes`, {
+                method: 'GET',
+                headers: {
+                  'PostID': postID
+                }
+            })
+            .then(response => response.json())
+            .then(data => setLikes(data.Value));
+        }
+    }
     
 
     useEffect(() => {
+
+        fetchLikes();
         
         //fetch author info
         if(authorID != null){
@@ -153,17 +171,7 @@ const Post = (props) => {
         .then(data => setAuthorData(data.Value));
         }
 
-        //fetch likes info
-        if(postID!=null){
-            fetch(`${baseUrl}/postLikes`, {
-                method: 'GET',
-                headers: {
-                  'PostID': postID
-                }
-            })
-            .then(response => response.json())
-            .then(data => setLikes(data.Value));
-        }
+        
 
         likes.forEach(element => {
             if(element.UserId==loggedInId){
@@ -292,7 +300,7 @@ const Post = (props) => {
                             <div className='like-modal-content-list'>
                              {likes.map((liker) => {
                                   return (<LikerCard
-                                    key={liker.Id}
+                                    key={liker.Liker_ID}
                                     username={liker.UserName}
                                     profilePic={liker.ProfilePicture}
                                     firstName={liker.FirstName}
